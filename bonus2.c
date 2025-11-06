aaa7c8ea8117d44f88b794ee47e0f3a93121029c
Lucas Buschbeck <esnichso@protonmail.com>
2025-11-06 13:31:34 +0100

    Bonus 2

---
+++ bonus2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+long power10(int x) 
+{
+    long o = 1;
+    for (int i = 0; i < x; i++) {
+        o = o * 10;
+    }
+    return o;
+}
+
+int ziffern(int z[], int länge, long n) 
+{
+    for (int i = 0; i < länge; i++) {
+        z[i] = n / power10(länge - i-1);
+        n -= z[i] * power10(länge - i-1);
+    }
+    return 0;
+}
+
+int zahllänge(long x) {
+    int o = 0;
+    long i = 1;
+    
+    while (1) {
+        if (x > i)
+            o += 1;
+        else
+            return o;
+        i = i * 10;
+    }
+}
+
+int count(int liste[], int l, int x) {
+    int o = 0;
+    for (int i = 0; i < l; i++) {
+        if (liste[i] == x)
+            o += 1;
+    }
+    return o;
+}
+
+int main()
+{
+    long n;
+    printf("Zahl: ");
+    scanf("%ld", &n);
+    int länge = zahllänge(n);
+    
+    int z[länge];
+    ziffern(z, länge, n);
+
+    for (int i = 0; i <= 9; i++) {
+        printf("%d Anzahl: %d\n", i, count(z, länge, i));
+    }
+
+    return 0;
+}
